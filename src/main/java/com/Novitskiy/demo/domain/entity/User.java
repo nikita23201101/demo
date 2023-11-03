26fe63b768cf78ff17669609393b8a3018505a4d
Nikita <nikita23201101@gmail.com>
2023-11-03 17:44:47 +0300

    Новицкий Лаб 7 Подключение Bootstrap

--- src/main/java/com/Novitskiy/demo/domain/entity/User.java
+++ src/main/java/com/Novitskiy/demo/domain/entity/User.java
@@ -35,6 +35,8 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    public boolean isAdmin() {return roles.contains( Role.ADMIN );}
+
     public User(String username, String password, boolean active, Set<Role> roles) {
         this.username = username;
         this.password = password;
